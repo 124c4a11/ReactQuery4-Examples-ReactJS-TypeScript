88229eaf4b4a65042f23e222fd6ba1c5592099af
124c4a11 <124c4a11@mail.ru>
2023-04-11 13:06:32 +0300

    Handling mutation response

--- src/hooks/useAddHeroData.ts
+++ src/hooks/useAddHeroData.ts
@@ -1,6 +1,6 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { QueryCache, useMutation, useQueryClient } from "@tanstack/react-query";
 import { IHero } from "../types/IHero";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 const addHero = (hero: Omit<IHero, "id">) =>
   axios.post<IHero>(`http://localhost:4000/superheroes`, hero);
@@ -10,6 +10,19 @@ export const useAddHeroData = () => {
 
   return useMutation({
     mutationFn: addHero,
-    onSuccess: () => queryClient.invalidateQueries(["heroes"]),
+    onSuccess: (data) => {
+      // queryClient.invalidateQueries(["heroes"]);
+
+      queryClient.setQueryData<AxiosResponse<IHero[]> | undefined>(
+        ["heroes"],
+        (oldQueryData) =>
+          oldQueryData
+            ? {
+                ...oldQueryData,
+                data: [...oldQueryData.data, data.data],
+              }
+            : oldQueryData
+      );
+    },
   });
 };
